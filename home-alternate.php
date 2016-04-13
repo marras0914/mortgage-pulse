9fe8546435a5e6684cc416fa78cafd8877dd2a93
Arras <MArras@loanpacific.com>
2016-04-13 15:59:34 -0500

    tighted up mobile

--- home-alternate.php
+++ home-alternate.php
@@ -11,9 +11,9 @@ Template Name: Home Alternate
 <div class="home-alternate-container" style="">
     
 	<div class="notes-container" style="height:600px; z-index:10; width:85%; display:inline-block; margin-top:15%; text-align:center;">
-	    <img src="http://blog.pacificunionfinancial.com/wp-content/uploads/2016/03/Mortgage-Notes-2.png"><br />
+	    <img src="http://blog.pacificunionfinancial.com/wp-content/uploads/2016/03/Mortgage-Notes-2.png">
 
-		<p style="padding-top:20px;line-height:1.2em;font-family: 'Droid Serif', serif;font-size:38px;color:#fff;">Mortgage Notes</p>
+		<p class="notes-title">Mortgage Notes</p>
 
 		<p style="font-family: 'Open Sans', sans-serif;font-size:18px;color:#fff;">Need news of note from across the mortgage industry?<br />We've got you covered.</p>
 
@@ -23,21 +23,24 @@ Template Name: Home Alternate
 
 			<div class="notes house-note" style="">
 				<div style="float:none;height:17px;background-color:#f1f2f2;"></div>
-				<div style="height:190px;background-color:#f1f2f2;"><p style="padding-top:15px;font-family: 'Open Sans', sans-serif;font-size:18px; font-weight:700;color:#414042;">HOUSE NOTES</p>
+				<div class="notes-content" style="">
+					<p class="notes-sub-title" style="">HOUSE NOTES</p>
 					<p style="color:#686868;font-size:14px;">Buying or refinancing?<br /> Here's extra info.</p>
 					<a href="http://blog.pacificunionfinancial.com/category/house-notes/" style="color:#1564a4;font-weight:700;text-decoration:none;font-size:14px;">READ MORE</a>
 				</div>
 			</div>
 			<div class="notes company-note" style="">
 				<div class="cut-out-arrow" style=""></div>
-				<div style="height:190px;background-color:#f1f2f2;"><p style="padding-top:15px;font-family: 'Open Sans', sans-serif;font-size:18px; font-weight:700;color:#414042;">COMPANY NOTES</p>
+				<div class="notes-content" style="">
+					<p class="notes-sub-title" style="">COMPANY NOTES</p>
 					<p style="color:#686868;font-size:14px;">Here's what's happening<br /> inside Pacific Union.</p>
 					<a href="http://blog.pacificunionfinancial.com/category/company-notes/" style="color:#1564a4;font-weight:700;text-decoration:none;font-size:14px;">READ MORE</a>
 				</div>
 			</div>
 			<div class="notes key-note" style="">
 				<div style="float:none;height:17px;background-color:#f1f2f2;"></div>
-				<div style="height:190px;background-color:#f1f2f2;"><p style="padding-top:15px;font-family: 'Open Sans', sans-serif;font-size:18px; font-weight:700;color:#414042;">KEY NOTES</p>
+				<div class="notes-content" style="">
+					<p class="notes-sub-title" style="">KEY NOTES</p>
 					<p style="color:#686868;font-size:14px;">Industry insight and<br />intelligence curated for you.</p>
 					<a href="http://blog.pacificunionfinancial.com/category/key-notes/" style="color:#1564a4;font-weight:700;text-decoration:none;font-size:14px;">READ MORE</a>
 				</div>
